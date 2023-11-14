ce3983291065d89187075165ffefe1cd1068641c
Osho.Jacob <osho.jacob@unity-bars.com>
2023-11-14 19:01:00 -0300

    finished with models

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Recepie } from '../recepie.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recepie[] = [
+    new Recepie(
+      'A Test',
+      'this is a test',
+      'https://res.cloudinary.com/oilsbeatstore/image/upload/v1675114887/Cancelled%20Visa/Cancelled%20Visa_Img.jpg'
+    ),
+    new Recepie(
+      'A Test',
+      'this is a test',
+      'https://res.cloudinary.com/oilsbeatstore/image/upload/v1675114887/Cancelled%20Visa/Cancelled%20Visa_Img.jpg'
+    ),
+    new Recepie(
+      'A Test',
+      'this is a test',
+      'https://res.cloudinary.com/oilsbeatstore/image/upload/v1675114887/Cancelled%20Visa/Cancelled%20Visa_Img.jpg'
+    ),
+    new Recepie(
+      'A Test',
+      'this is a test',
+      'https://res.cloudinary.com/oilsbeatstore/image/upload/v1675114887/Cancelled%20Visa/Cancelled%20Visa_Img.jpg'
+    ),
+    new Recepie(
+      'A Test',
+      'this is a test',
+      'https://res.cloudinary.com/oilsbeatstore/image/upload/v1675114887/Cancelled%20Visa/Cancelled%20Visa_Img.jpg'
+    ),
+  ];
 }
